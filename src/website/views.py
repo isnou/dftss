1c9167d2a476be85d8341f0bcea630bb6afe75a7
isnou <nouioua0islem@gmail.com>
2022-03-26 00:57:34 +0100

    cimmit

--- src/website/views.py
+++ src/website/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from django.shortcuts import render,redirect
+from website.forms import ContactUs
+from django.core.mail import BadHeaderError, EmailMessage
+from django.conf import settings
+
 # Create your views here.
+def index(request):
+    form = ContactUs(request.POST or None)
+    if form.is_valid():
+        subject = 'Subject:' + form.cleaned_data['subject']
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        from_email = '{name} <{email}>'.format(name=name, email=email)
+        message = form.cleaned_data['message']
+        recipient_list = [settings.EMAIL_HOST_USER]
+        email_message = EmailMessage(subject, message, from_email, recipient_list, reply_to=[email])
+        try:
+            email_message.send()
+        except BadHeaderError:
+            return HttpResponse('Un en-tête non valide a été détecté.')
+    return render(request, "base.html",{})
