f3678b5ca75042221ba7ad9091311107f5c60250
isnou <nouioua0islem@gmail.com>
2022-03-29 04:11:18 +0100

    update admin

--- src/website/admin.py
+++ src/website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project, Service, Partner, Team, Counter, Content
 
-# Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Service)
+admin.site.register(Partner)
+admin.site.register(Team)
+admin.site.register(Counter)
+admin.site.register(Content)
