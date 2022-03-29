e08c76c788a642c9edb1f3b11e3767af6f6eef27
isnou <nouioua0islem@gmail.com>
2022-03-29 04:54:48 +0100

    update view

--- src/website/views.py
+++ src/website/views.py
@@ -4,9 +4,42 @@ from django.shortcuts import render,redirect
 from website.forms import ContactUs
 from django.core.mail import BadHeaderError, EmailMessage
 from django.conf import settings
+from django.http import Http404
+from .models import Project, Service, Partner, Team, Counter, Content
 
 # Create your views here.
 def index(request):
+
+    try:
+        content = Content.objects.filter(profile_name="English Profile")
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
+
+    try:
+        project = Project.objects.all()
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
+
+    try:
+        service = Service.objects.all()
+    except Service.DoesNotExist:
+        raise Http404("Service does not exist")
+
+    try:
+        partner = Partner.objects.all()
+    except Partner.DoesNotExist:
+        raise Http404("Partner does not exist")
+
+    try:
+        team = Team.objects.all()
+    except Project.DoesNotExist:
+        raise Http404("Team does not exist")
+
+    try:
+        counter = Counter.objects.all()
+    except Counter.DoesNotExist:
+        raise Http404("Counter does not exist")
+
     form = ContactUs(request.POST or None)
     if form.is_valid():
         subject = 'Subject:' + form.cleaned_data['subject']
@@ -20,4 +53,14 @@ def index(request):
             email_message.send()
         except BadHeaderError:
             return HttpResponse('Un en-tête non valide a été détecté.')
-    return render(request, "base.html",{})
+
+    context = {
+        'content': content,
+        'projects': project,
+        'service': service,
+        'partner': partner,
+        'team': team,
+        'counter': counter
+    }
+
+    return render(request, "base.html", context)
