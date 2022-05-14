a153151ffdd51fdcb7a10d7f3cd6b3a34a49e4ff
isnou <nouioua0islem@gmail.com>
2022-05-14 14:10:45 +0100

    repair dftss

--- src/dexunt/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.shortcuts import render,redirect
-from django.http import Http404
-
-
-def main(request):
-
-    return render(request, "base_layout.html")
