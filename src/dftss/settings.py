047572961dc41e2562c9c4eb9046c7842ff36a85
isnou <nouioua0islem@gmail.com>
2022-05-14 15:20:14 +0100

    debug

--- src/dftss/settings.py
+++ src/dftss/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-s$v9^2p7b@3mm)%(ilumfe-)jyh5*5@41!ll=b0=%(nojjc)*h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
