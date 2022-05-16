601a87e9f1e1979bf7d33da71563255ea2fb52d3
isnou <nouioua0islem@gmail.com>
2022-05-16 23:35:14 +0100

    slide slider

--- src/dexunt/admin.py
+++ src/dexunt/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Product, ProductImage, Category, Tag, Slider, Banner
+from .models import Product, ProductImage, Category, Tag, Slide, Banner
 
 admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Slider)
+admin.site.register(Slide)
 admin.site.register(Banner)
 
 
