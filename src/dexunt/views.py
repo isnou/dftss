782a086fcce429a5bcdf69111d0b1f940ab8244c
isnou <nouioua0islem@gmail.com>
2022-05-16 05:51:15 +0100

    tst

--- src/dexunt/views.py
+++ src/dexunt/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
+from .models import Product, Category
 
 
 def home(request):
-    return render(request, "dexunt/home.html")
+    try:
+        products = Product.objects.all()
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+
+    try:
+        categories = Category.objects.all()
+    except Category.DoesNotExist:
+        raise Http404("Category does not exist")
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, "dexunt/home.html", context)
 
