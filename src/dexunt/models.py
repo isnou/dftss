d70d7d9d3c5ac0da2ecc36823960843d65060d9a
isnou <nouioua0islem@gmail.com>
2022-05-16 04:52:12 +0100

    no integer

--- src/dexunt/models.py
+++ src/dexunt/models.py
@@ -1,4 +1,4 @@
-from django.db import models, IntegerField
+from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
