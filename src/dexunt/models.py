bd38bc8fba9984339b28cedf7219f54b11ac33ab
isnou <nouioua0islem@gmail.com>
2022-05-16 23:25:27 +0100

    sttts

--- src/dexunt/models.py
+++ src/dexunt/models.py
@@ -28,15 +28,14 @@ class Tag(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     sku = models.CharField(max_length=300, unique=True)
-    main_image = models.ImageField(upload_to='dexunt/main/image/')
-    slider_image = models.ImageField(upload_to='dexunt/slider/image/')
     images = models.ManyToManyField(ProductImage, blank=True)
-    category = models.ForeignKey('Category',on_delete=models.CASCADE)
-    tag = models.ForeignKey('Tag',on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
     description = models.TextField(max_length=800)
     specification = models.TextField(max_length=800)
     catch_line = models.CharField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True)
     rate = models.IntegerField(
         default=0,
         validators=[
@@ -47,3 +46,37 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Slide(models.Model):
+    CHOICES = (
+        ('FIRST', 'FIRST'),
+        ('SECOND', 'SECOND'),
+        ('THIRD', 'THIRD'),
+    )
+    image = models.ImageField(upload_to='dexunt/slides/')
+    small_text = models.CharField(max_length=50, blank=True)
+    big_text = models.CharField(max_length=50, blank=True)
+    button = models.CharField(max_length=50, blank=True)
+    link = models.URLField(blank=True)
+    choice = models.CharField(max_length=50, choices=CHOICES, blank=True)
+
+    def __str__(self):
+        return self.link
+
+
+class Banner(models.Model):
+    CHOICES = (
+        ('FIRST', 'FIRST'),
+        ('SECOND', 'SECOND'),
+        ('THIRD', 'THIRD'),
+    )
+    image = models.ImageField(upload_to='dexunt/banners/')
+    small_text = models.CharField(max_length=50, blank=True)
+    big_text = models.CharField(max_length=50, blank=True)
+    button = models.CharField(max_length=50, blank=True)
+    link = models.URLField(blank=True)
+    choice = models.CharField(max_length=50, choices=CHOICES, blank=True)
+
+    def __str__(self):
+        return self.link
