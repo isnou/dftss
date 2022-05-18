ac0353cf5664c7e92eb8ca5db07c1cee8267d1fd
isnou <nouioua0islem@gmail.com>
2022-05-18 18:08:49 +0100

    gg

--- src/dexunt/admin.py
+++ src/dexunt/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, ItemImage, ProductImage, Category, Tag, Slide, Banner, Option
+from .models import Item, ItemImage, Category, Tag, Slide, Banner, Option
 
 admin.site.register(Item)
 admin.site.register(ItemImage)
