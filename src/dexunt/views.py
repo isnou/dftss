2c00262d8e3f61759a0b03daaa234195ec218886
isnou <nouioua0islem@gmail.com>
2022-05-18 23:00:53 +0100

    ff

--- src/dexunt/views.py
+++ src/dexunt/views.py
@@ -58,7 +58,7 @@ def detail(request, key_id):
     tags = item.tag.all()
 
     for tag in tags:
-        related_items = Item.objects.filter(tag=tag)
+        related_items = Item.all(tag=tag)
 
     context = {
         'item': item,
