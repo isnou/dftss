7981dcd57c66065e8835fa0c0977d0d7d0485dcd
isnou <nouioua0islem@gmail.com>
2022-05-18 18:02:15 +0100

    ff

--- src/dexunt/models.py
+++ src/dexunt/models.py
@@ -3,12 +3,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class ProductImage(models.Model):
-    name = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='dexunt/images/')
+class ItemImage(models.Model):
+    album_name = models.CharField(max_length=200, blank=True)
+    album = models.ImageField(upload_to='dexunt/images/')
 
     def __str__(self):
-        return self.name
+        return self.album_name
 
 
 class Category(models.Model):
@@ -25,20 +25,51 @@ class Tag(models.Model):
         return self.name
 
 
+class Option(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
+    SHOE = (
+        ('EU 35', 'EU 35'),
+        ('EU 36', 'EU 36'),
+        ('EU 37', 'EU 37'),
+        ('EU 38', 'EU 38'),
+        ('EU 39', 'EU 39'),
+        ('EU 40', 'EU 40'),
+        ('EU 41', 'EU 41'),
+        ('EU 42', 'EU 42'),
+        ('EU 43', 'EU 43'),
+        ('EU 44', 'EU 44'),
+    )
+    CLOTHING = (
+        ('XXS', 'XXS'),
+        ('XS', 'XS'),
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+        ('XXL', 'XXL'),
+    )
     name = models.CharField(max_length=200)
     sku = models.CharField(max_length=300, unique=True)
+    shoe_size = models.CharField(max_length=50, choices=SHOE, blank=True, null=True)
+    clothing_size = models.CharField(max_length=50, choices=CLOTHING, blank=True, null=True)
     image = models.ImageField(upload_to='dexunt/slides/')
-    images = models.ManyToManyField(ProductImage, blank=True)
+    images = models.OneToManyField(ItemImage, blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
+    option = models.ForeignKey('Option', on_delete=models.CASCADE)
     description = models.TextField(max_length=800)
     specification = models.TextField(max_length=800)
     catch_line = models.CharField(max_length=200, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     discount_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     rate = models.IntegerField(
-        default=0,
+        default=5,
         validators=[
             MaxValueValidator(5),
             MinValueValidator(0)
