8d976c6c2fec5a62c717228313678d368213eca0
isnou <nouioua0islem@gmail.com>
2022-05-19 00:45:34 +0100

    lm

--- src/dexunt/urls.py
+++ src/dexunt/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('<int:key_id>/detail/', views.detail, name="detail"),
-    path('load/', views.load_more, name='load'),
 ]
