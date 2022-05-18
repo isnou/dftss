43d7a0fe367ccf8843d3524efe384d111dbcc734
isnou <nouioua0islem@gmail.com>
2022-05-18 19:17:27 +0100

    admin

--- src/dexunt/admin.py
+++ src/dexunt/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, ItemImage, Category, Tag, Slide, Banner, Option, Shoe, Clothing
+from .models import Item, ItemImage, Category, Tag, Slide, Banner, Option, Shoe, Clothing, Color
 
 admin.site.register(Item)
 admin.site.register(ItemImage)
@@ -10,3 +10,4 @@ admin.site.register(Banner)
 admin.site.register(Option)
 admin.site.register(Shoe)
 admin.site.register(Clothing)
+admin.site.register(Color)
