0380ddb62c644a516ce13283b521e16bbfa3f92a
isnou <nouioua0islem@gmail.com>
2022-05-19 01:19:11 +0100

    gg

--- src/dexunt/urls.py
+++ src/dexunt/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('<int:key_id>/detail/', views.detail, name="detail"),
-    path('posts-json/<int:num_posts>/', views.PostJsonListView, name='posts-json-view'),
+    path('posts-json/<int:num_posts>/', PostJsonListView.as_view(), name='posts-json-view'),
 ]
