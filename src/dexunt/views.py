23f5f289aa70a336b6bf48f3c4247e46eedae7db
isnou <nouioua0islem@gmail.com>
2022-05-21 03:53:20 +0100

    kjnj

--- src/dexunt/views.py
+++ src/dexunt/views.py
@@ -187,9 +187,9 @@ def latest_products(request):
 
 
 def shopping_cart(request, key_id):
-    size = request.GET.get('size')
-    color = request.GET.get('color')
-    option = request.GET.get('option')
+    size = 5
+    color = ('color')
+    option = 'fr'
 
     try:
         items = Item.objects.all().order_by('-id')
