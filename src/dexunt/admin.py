32b4dc7ca91233d91cb31efe1a47dee045cafa73
isnou <nouioua0islem@gmail.com>
2022-05-22 02:18:19 +0100

    hbmh

--- src/dexunt/admin.py
+++ src/dexunt/admin.py
@@ -17,7 +17,7 @@ class ShopAdmin(admin.ModelAdmin):
 
 class PreOrderAdmin(admin.ModelAdmin):
     search_fields = []
-    list_display = ('get_products', 'color', 'option', 'shoe_size', 'clothing_size')
+    list_display = ('product_name', 'product_sku', 'color', 'option', 'shoe_size', 'clothing_size')
 
 
 
