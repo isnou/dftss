bad8be2e7fb4ef5d4418bc52bb98967061a67fae
isnou <nouioua0islem@gmail.com>
2022-05-22 21:48:52 +0100

    khh

--- src/dexunt/models.py
+++ src/dexunt/models.py
@@ -35,6 +35,13 @@ class SubCategory(models.Model):
         return self.slug
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
 class Brand(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.CharField(max_length=200, blank=True, null=True)
@@ -83,12 +90,14 @@ class Item(models.Model):
     images = models.ManyToManyField(ItemImage, blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
     sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE, blank=True, null=True)
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE, blank=True, null=True)
     brand = models.ForeignKey('Brand', on_delete=models.CASCADE, blank=True, null=True)
     description = models.TextField(max_length=800)
     specification = models.TextField(max_length=800)
     catch_line = models.CharField(max_length=200, blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     old_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    delivery_price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     sell_rate = models.IntegerField(default=0)
     rate = models.IntegerField(
         default=5,
@@ -150,3 +159,15 @@ class Shop(models.Model):
     def __str__(self):
         return self.name
 
+
+class ShoppingCart(models.Model):
+    product_id = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.DecimalField(max_digits=8, decimal_places=2, default=1)
+    shoe_size = models.ManyToManyField(Shoe, blank=True)
+    clothing_size = models.ManyToManyField(Clothing, blank=True)
+    color = models.ManyToManyField(Color, blank=True)
+    option = models.ManyToManyField(Option, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
