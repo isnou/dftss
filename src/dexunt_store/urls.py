fc3a2035f4754b92d0da95919f3c9766d622dab2
isnou <nouioua0islem@gmail.com>
2022-05-24 00:59:30 +0100

    dfde

--- src/dexunt_store/urls.py
+++ src/dexunt_store/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('<int:collection>/store-detail/', views.store_detail, name="store_detail"),
+    path('<str:collection>/store-detail/', views.store_detail, name="store_detail"),
 
 ]
