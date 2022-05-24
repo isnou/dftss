796b6bbf322f1266e32549dcf671e2a705766c71
isnou <nouioua0islem@gmail.com>
2022-05-24 12:18:53 +0100

    bool

--- src/dexunt_store/models.py
+++ src/dexunt_store/models.py
@@ -117,7 +117,7 @@ class Product(models.Model):
         ]
     )
     collection = models.CharField(max_length=50, choices=COLLECT, null=True)
-    publish = models.BooleanField(initial=True)
+    publish = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
