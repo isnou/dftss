fb92b05c87cad919519f9d4029b6c552279ae411
isnou <nouioua0islem@gmail.com>
2022-05-25 00:58:52 +0100

    now

--- src/dexunt_sell/admin.py
+++ src/dexunt_sell/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, Delivery, Destination, SubDestination
+from .models import Order, Shipping, Destination, SubDestination
 
 
 class OrderAdmin(admin.ModelAdmin):
