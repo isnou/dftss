72abaa921355a1978045c32f1fedd7a5676b17ae
isnou <nouioua0islem@gmail.com>
2022-05-24 19:14:14 +0100

    ff

--- src/dexunt_store/apps.py
+++ src/dexunt_store/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DexuntStoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dexunt-store'
+    name = 'dexunt_store'
