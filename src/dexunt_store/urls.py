801841dfd98c5c3ffd33efba3d83d4ac4b3749b5
isnou <nouioua0islem@gmail.com>
2022-05-24 01:04:41 +0100

    hbh

--- src/dexunt_store/urls.py
+++ src/dexunt_store/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('<str:collection>/store-detail/', views.store_detail, name="store_detail"),
+    path('<str:collection>/store-detail/', views.store_detail, name="store-detail"),
 
 ]
