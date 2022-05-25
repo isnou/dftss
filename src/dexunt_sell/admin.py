266cb2fa442cc99ea4f669900b34a6688fd2bf01
isnou <nouioua0islem@gmail.com>
2022-05-25 01:00:43 +0100

    shipping

--- src/dexunt_sell/admin.py
+++ src/dexunt_sell/admin.py
@@ -7,13 +7,13 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = (
         'order_ref', 'order_date', 'order_state', 'client_name', 'client_phone', 'registered_client', 'product_sku',
         'product_color', 'product_option', 'product_shoe_size', 'product_clothing_size', 'product_price',
-        'delivery_price',
-        'delivery_destination', 'payment_method')
+        'shipping_price',
+        'shipping_destination', 'payment_method')
     list_filter = (
         'order_ref', 'order_date', 'order_state', 'client_name', 'client_phone', 'registered_client', 'product_sku',
         'product_color', 'product_option', 'product_shoe_size', 'product_clothing_size', 'product_price',
-        'delivery_price',
-        'delivery_destination', 'payment_method')
+        'shipping_price',
+        'shipping_destination', 'payment_method')
 
 
 class ShippingAdmin(admin.ModelAdmin):
