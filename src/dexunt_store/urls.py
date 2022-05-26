4cf06f58ebd3736ed52906e43fe2c7e9bf2443be
isnou <nouioua0islem@gmail.com>
2022-05-26 22:31:38 +0100

    Update urls.py

--- src/dexunt_store/urls.py
+++ src/dexunt_store/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<str:collection>/store-detail/', views.store_detail, name="store-detail"),
     path('<int:product_id>/product-detail/', views.product_detail, name="product-detail"),
     path('<str:product_sku>/shopping-cart/', views.shopping_cart, name="shopping-cart"),
-    path('<str:order_ref>/', views.new_order_home, name="new-order-home"),
+    path('<str:order_ref>/<str:group_order_ref>/', views.new_order_home, name="new-order-home"),
     path('<str:order_ref>/check-out/', views.check_out, name="check-out"),
 ]
