8f308e6f568dd37cf8ff8b84db606455542385ac
isnou <nouioua0islem@gmail.com>
2022-05-28 12:24:23 +0100

    (numProduct-1)

--- site/public/static/dexunt-store/js/main.js
+++ site/public/static/dexunt-store/js/main.js
@@ -217,15 +217,15 @@
     $('.btn-num-product-down').on('click', function(){
         var numProduct = Number($(this).next().val());
         var numPrice = document.getElementById("price").value;
-        if(numProduct > 2) $(this).next().val(numProduct - 1);
-        document.getElementById("total").innerHTML = numPrice * numProduct;
+        if(numProduct > 1) $(this).next().val(numProduct - 1);
+        document.getElementById("total").innerHTML = numPrice * (numProduct-1);
     });
 
     $('.btn-num-product-up').on('click', function(){
         var numProduct = Number($(this).prev().val());
         var numPrice = document.getElementById("price").value;
         $(this).prev().val(numProduct + 1);
-        document.getElementById("total").innerHTML = numPrice * numProduct;
+        document.getElementById("total").innerHTML = numPrice * (numProduct+1);
     });
 
     /*==================================================================
