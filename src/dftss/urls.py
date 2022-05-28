0ba5aa37463d3a7b0ea0ff24551be5be52ab760d
isnou <nouioua0islem@gmail.com>
2022-05-28 04:58:56 +0100

    path('dexunt/', include('dexunt_store.urls')),

--- src/dftss/urls.py
+++ src/dftss/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('website.urls')),
     path('admin/', admin.site.urls),
-    path('dexunt/', include('dexunt_store.urls')),
     path('dexunt-store/', include('dexunt_store.urls')),
 ]
