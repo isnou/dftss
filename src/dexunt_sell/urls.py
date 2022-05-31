18bcdd6618a5c6d01ae65b667f6c9e789cb032b7
isnou <nouioua0islem@gmail.com>
2022-05-31 13:39:58 +0100

    df

--- src/dexunt_sell/urls.py
+++ src/dexunt_sell/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.manager_home, name="manager-home"),
     path('orders-list/', views.orders_list, name="orders-list"),
     path('<str:group_order_ref>/orders-details/', views.orders_details, name="orders-details"),
-    path('<str:group_order_ref>/<str:order_ref>/orders-delete/', views.delete_order, name="delete-order"),
     path('<str:group_order_ref>/delete-orders/', views.delete_orders, name="delete-orders"),
-
+    path('<str:group_order_ref>/<str:order_ref>/delete-order/', views.delete_order, name="delete-order"),
 ]
