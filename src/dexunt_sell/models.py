418291aa94f0e3c60354cd8bfc53629267d93f3b
isnou <nouioua0islem@gmail.com>
2022-06-01 03:34:35 +0100

    money-bill-wave

--- src/dexunt_sell/models.py
+++ src/dexunt_sell/models.py
@@ -46,7 +46,7 @@ class GroupOrder(models.Model):
         ('REMOVED', 'REMOVED'),
         ('REQUEST', 'REQUEST'),
         ('UNPAID', 'UNPAID'),
-        ('PAYED', 'PAYED'),
+        ('PAID', 'PAID'),
         ('PEND', 'PEND'),
     )
 
