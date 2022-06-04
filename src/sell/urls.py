ec34725780d460ccf411c62b527468323830d438
isnou <nouioua0islem@gmail.com>
2022-06-04 20:53:22 +0100

    sell-products-list

--- src/sell/urls.py
+++ src/sell/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="sell-home"),
-    path('products/', views.home, name="products-list"),
+    path('products/', views.home, name="sell-products-list"),
 ]
