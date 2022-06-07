c3a61feeb2b8ecc96fb847e9777f643c7dd07c6c
isnou <nouioua0islem@gmail.com>
2022-06-07 22:35:49 +0100

    hh

--- src/store/urls.py
+++ src/store/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="store-home"),
     path('<str:collection>/store/', views.store, name="store"),
+    path('<int:product_id>/product/', views.product, name="product"),
 ]
