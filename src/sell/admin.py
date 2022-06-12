097dd2e62e35cc799716d88cd26cfd8ca802cecb
isnou <nouioua0islem@gmail.com>
2022-06-13 00:48:45 +0100

    Update admin.py

--- src/sell/admin.py
+++ src/sell/admin.py
@@ -3,7 +3,7 @@ from .models import Order, Item
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('ref', 'state', 'item', 'session_id', 'client_name', 'client_phone', 'registered_client',
+    list_display = ('ref', 'state', 'items', 'session_id', 'client_name', 'client_phone', 'registered_client',
                     'coupon_value', 'coupon_code', 'shipping_destination', 'shipping_price', 'total_price')
 
 
