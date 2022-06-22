88adecde253340256195696f433ca9cc989efa58
isnou <nouioua0islem@gmail.com>
2022-06-22 18:35:34 +0100

    Update models.py

--- src/store/models.py
+++ src/store/models.py
@@ -70,6 +70,7 @@ class Option(models.Model):
     type = models.CharField(max_length=50, choices=TYPES, default='PACK')
     title = models.CharField(max_length=200, blank=True)
     parameter = models.ManyToManyField(Parameter, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
 
     def get_parameters(self):
         return "\n".join([p.value for p in self.parameter.all()])
