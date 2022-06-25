c17a2cb60df41f721ef77ea5aacabb8b6acc3091
isnou <nouioua0islem@gmail.com>
2022-06-25 17:24:13 +0100

    Update models.py

--- src/sell/models.py
+++ src/sell/models.py
@@ -4,6 +4,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Item(models.Model):
     sku = models.CharField(max_length=200)
+    ref = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=200, default='UNDEFINED')
     image = models.ImageField(upload_to='dexunt-sotre/products', blank=True)
     color = models.CharField(max_length=200, default='UNDEFINED')
