85c1caf82dcccec1a5f1349801ebc7000a8205a4
isnou <nouioua0islem@gmail.com>
2022-07-04 19:41:08 +0100

    home

--- src/store_manager/urls.py
+++ src/store_manager/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="store-home"),
+    path('', views.home, name="store"),
+    path('', views.home, name="product"),
+    path('', views.home, name="order"),
+    path('', views.home, name="delete-product"),
+    path('', views.home, name="add-quantity"),
+    path('', views.home, name="remove-quantity"),
+    path('', views.home, name="delete-product-to-home"),
+    path('', views.home, name="show-cart"),
 ]
