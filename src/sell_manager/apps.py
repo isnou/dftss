e78058a32ae85baf225f08a78c4da35c7fbbcf72
isnou <nouioua0islem@gmail.com>
2022-07-04 13:23:28 +0100

    tst

--- src/sell_manager/apps.py
+++ src/sell_manager/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SellConfig(AppConfig):
+class SellManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sell_manager'
