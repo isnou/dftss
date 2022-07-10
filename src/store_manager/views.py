5caa1205c5cd79cbce3a365c678baf70d32f208b
isnou <nouioua0islem@gmail.com>
2022-07-10 22:42:27 +0100

    Update views.py

--- src/store_manager/views.py
+++ src/store_manager/views.py
@@ -120,7 +120,9 @@ def product(request, product_id):
     tags = selected_product.tag.split()
     related_products = []
     for tag in tags:
-        related_products = all_products.filter(tag__contains=tag)
+        related_product = all_products.filter(tag__contains=tag)
+        if related_product not in related_products:
+            related_products += related_product
 
     # related_products = all_products.filter(
     #    Q(filter=selected_product.filter) | Q(flip=selected_product.filter))
