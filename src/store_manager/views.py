e72130b11e9feaf6f09d2cc2da709071cbd1bb39
isnou <nouioua0islem@gmail.com>
2022-07-15 17:30:07 +0100

    Update views.py

--- src/store_manager/views.py
+++ src/store_manager/views.py
@@ -123,11 +123,10 @@ def product(request, product_id):
         options = None
 
     if not relations.filter(name=selected_product.name).exists():
-        new = Relation(name=selected_product.name)
-        new.save()
+        new = Relation(name=selected_product.name).save()
         tags = selected_product.tag.split()
         for tag in tags:
-            new.add(all_products.filter(tag__contains=tag))
+            new.product.add(all_products.filter(tag__contains=tag))
 
     related_products = relations.filter(name=selected_product.name).produtc.all()
     # related_products = all_products.filter(
